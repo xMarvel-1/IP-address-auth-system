a01ac8fffa53b030e2f2a4c6101d1b554d35b20c
xMarvel <72876863+xMarvel-1@users.noreply.github.com>
2021-03-21 09:06:25 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from colorama import Fore, init, Style
+import requests
+import ctypes
+import os
+
+def Auth():
+    IP = requests.get('http://api.ipify.org/').text
+    r = requests.get('https://pastebin.com/raw/...............').text##Your raw link
+    if IP in r:
+        print('Matched !')
+        pass
+    else:
+        os.system('cls')
+        print(Fore.RED + '\n -- ' + Fore.WHITE + Style.BRIGHT + 'IP address unauthorized!')
+        print(Style.RESET_ALL + Fore.RED + ' -- ' + Fore.WHITE + Style.BRIGHT + 'Your IP: ' + IP)
+        input()
+        quit()
+
+Auth()
